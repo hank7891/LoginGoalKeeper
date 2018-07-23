f8d74a90a454dca2a396c1f73d34d402dcdf99b7
Hank <hank@doubleservice.com>
2018-07-23 10:15:09 +0800

    feat: 實作證碼檢查套件 檢查綁定格式邏輯
    實作綁定格式邏輯。
    實作測試綁定格式邏輯。
    添加建構元檢查並傳入參數邏輯。

--- libs/Custon/Gatekeeper/Gatekeeper.php
+++ libs/Custon/Gatekeeper/Gatekeeper.php
@@ -2,16 +2,45 @@
 
 namespace Gatekeeper;
 
+use Exception;
+
 class Gatekeeper
 {
-    public function __construct()
-    {
+    /**
+     * 綁定號碼
+     * @var
+     */
+    protected $bindingNumber;
 
+    public function __construct($bindingNumber)
+    {
+        if (!self::checkBindingFormat($bindingNumber)) {
+            throw new Exception("綁定號碼格式錯誤", 1);
+        }
+
+        $this->bindingNumber = $bindingNumber;
     }
 
+    /**
+     * TODO 待實作
+     * @return mixed
+     */
     public function check()
     {
-        return true;
+        return $this->bindingNumber;
     }
 
+    /**
+     * 檢測綁定號碼格式
+     *
+     * @param $testedNumber
+     *
+     * @return int
+     */
+    public static function checkBindingFormat($testedNumber)
+    {
+        $format = '/^0\d{9}$/';
+
+        return (preg_match($format, $testedNumber));
+    }
 }
--- tests/GatekeeperTest .php
+++ tests/GatekeeperTest .php
@@ -4,9 +4,21 @@ use Gatekeeper\Gatekeeper;
 
 class GatekeeperTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * 測試 資料審核
+     * TODO 待實作
+     */
     public function testCheck()
     {
-        $gateKeeper = new Gatekeeper();
-        $this->assertEquals(true,$gateKeeper->check());
+        $gateKeeper = new Gatekeeper('0987385482');
+        $this->assertEquals('0987385482',$gateKeeper->check());
+    }
+
+    /**
+     * 測試 檢查綁定號碼格式
+     */
+    public function testCheckBindingFormat()
+    {
+        $this->assertEquals(true, Gatekeeper::checkBindingFormat('0987385482'));
     }
 }
